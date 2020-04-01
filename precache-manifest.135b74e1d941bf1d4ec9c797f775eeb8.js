89ccb628361126db61312067875b9674b125feff
varma1247 <saivarma10@gmail.com>
2020-03-31 20:38:56 -0500

    Updates

---
+++ precache-manifest.135b74e1d941bf1d4ec9c797f775eeb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66489a23191f8b45b78dcfe719c4c633",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "bbb0f82a6105dabd778e",
+    "url": "/coronatracker/static/css/main.2d214b5e.chunk.css"
+  },
+  {
+    "revision": "4af15a27b725580ac32d",
+    "url": "/coronatracker/static/js/2.a5a61179.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coronatracker/static/js/2.a5a61179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb0f82a6105dabd778e",
+    "url": "/coronatracker/static/js/main.d618caf4.chunk.js"
+  },
+  {
+    "revision": "36e29ee7cf836b9d267e",
+    "url": "/coronatracker/static/js/runtime-main.0e04e4cd.js"
+  }
+]);
